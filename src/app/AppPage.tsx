012fa2bc96b8c01911cdc85dfdc8915f1c9f7b10
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-12-16 00:03:59 +0000

    feat(board): add strict types for board items

--- src/app/AppPage.tsx
+++ src/app/AppPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Canvas from '../components/Canvas/Canvas'
 
 const AppPage = () => {
---
+++ src/types/board.ts
@@ -0,0 +1,25 @@
+export type BoardItemBase = {
+    id: string;
+    x: number;
+    y: number;
+    zIndex: number;
+};
+
+export type ImageItem = BoardItemBase & {
+    type: "image";
+    src: string;
+    width: number;
+    height: number;
+};
+
+export type ColorItem = BoardItemBase & {
+    type: "color";
+    hex: string;
+};
+
+export type TextItem = BoardItemBase & {
+    type: "text";
+    text: string;
+};
+
+export type BoardItem = ImageItem | ColorItem | TextItem;
